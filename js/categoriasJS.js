d61a28540f0ebe5974cded19a391960535bc7926
ryelgr2611 <ryelgar2611@g.educaand.es>
2024-02-21 14:36:07 +0100

    clase

---
+++ js/categoriasJS.js
@@ -0,0 +1,44 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+// Obtener el elemento con la clase "container"
+const container = document.querySelector('.container');
+let contenido ="";
+const urlParams = new URLSearchParams(window.location.search);
+const idTipo = urlParams.get('tipo');
+console.log(idTipo);
+    // Realizar una solicitud para obtener el archivo JSON de las categorías
+    fetch('js/categorias.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(categoria => {
+            if (categoria.id === idTipo)
+            {
+                contenido +=     `
+                <div class="row flex justify-content-center">
+                                <div class="col-12">
+                                    <div class="coleccion flex justify-content-center">
+                                        <div class="imagen text-center">
+                                            <img src="${categoria.imagenes[1]}"class="img-fluid border border-0 rounded-circle">
+                                        </div>
+                                        <div class="row text-center">
+                                            <h1 class="title text-danger">${categoria.nombre}</h1>
+                                            <div class="social-media">
+                                                <div class="link-holder"></div>
+                                            </div>
+                                            <div class="divide"></div>
+                                            <div class="content text-danger">
+                                                ${categoria.contenido.parrafos.map(parrafo => `<p>${parrafo}</p>`).join('')}
+                                                <p><b><span>${categoria.contenido.nota}</span></b></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                `;
+            }
+            container.innerHTML = contenido;
+        });
+    })
+    .catch(error => console.error('Error al obtener los datos:', error));
+
+});
--- js/scriptMain.js
+++ js/scriptMain.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Obtener el elemento del carousel
     const carouselInner = document.querySelector('.carousel-inner');
+    const carouselControls = document.querySelectorAll('.carousel-control-prev, .carousel-control-next');
 
     // Realizar una solicitud para obtener el archivo JSON de las categorías
     fetch('js/categorias.json')
@@ -52,21 +53,29 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             
         });
+        // Ocultar los botones de control del carrusel si hay menos de 3 categorías recomendadas
+        if (categoriasRecomendadas.length <= 3) {
+            carouselControls.forEach(control => {
+                control.classList.add('d-none');
+            });
+        }
     })
     .catch(error => console.error('Error al obtener los datos:', error));
 
     // Función para generar el contenido de la tarjeta de la categoría
     function generarContenidoCategoria(categoria) {
         return `
-            <div class="col mt-4 ">
-                <div class="card border-0 shadow-sm ">
-                    <div class="card-body text-center ">
-                        <h5 class="card-title">${categoria.nombre}</h5>
-                        <img src="${categoria.imagenes[0]}" class="card-img-top" alt="...">
-                        <p class="card-text">${categoria.descripcion}</p>
-                    </div>
+        <div class="col mt-4 cartaRec">
+        <a href="../recomendados.html?tipo=${categoria.id}" class="card-link">
+            <div class="card border-0 shadow-sm">
+                <div class="card-body text-center">
+                    <h5 class="card-title">${categoria.nombre}</h5>
+                    <img src="${categoria.imagenes[0]}" class="card-img-top" alt="...">
+                    <p class="card-text">${categoria.descripcion}</p>
                 </div>
             </div>
+        </a>
+    </div>
         `;
     }
 });
